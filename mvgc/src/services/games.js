6e42e4890a7e35fa3cb880566631bcf1830211e0
joq tyyppi <eero.ojala@helsinki.fi>
2018-05-08 19:06:15 +0300

    Added platforms-, games- and loginservice

---
+++ mvgc/src/services/games.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const baseUrl = '/api/games'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+
+    return response.data
+}
+
+const create = async (content) => {
+    const response = await axios.post(baseUrl, content)
+
+    return response.data
+}
+
+export default { getAll, create }

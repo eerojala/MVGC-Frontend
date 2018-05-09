349aa7f8fb822783fdddfb9550a00fad25952516
joq tyyppi <eero.ojala@helsinki.fi>
2018-05-09 21:08:00 +0300

    Games list now uses reducers

--- mvgc/src/App.js
+++ mvgc/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Container } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-// Services 
-import platformService from './services/platforms'
-import gameService from './services/games'
-import loginService from './services/login'
+// Reducer functions
+import { platformInit } from './reducers/platformReducer'
+import { gameInit } from './reducers/gameReducer' 
 
 // Components
 import NavigationMenu from './components/NavigationMenu'
@@ -14,78 +14,10 @@ import GamesList from './components/GamesList'
 import LoginForm from './components/LoginForm'
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            platforms: [],
-            games: [],
-            user: null,
-            username: '',
-            password: '',
-            name: '',
-            creator: '',
-            year: ''
-        }
-    }
-
     componentDidMount() {
-        gameService.getAll().then(games => this.setState({ games }))
-        platformService.getAll().then(platforms => this.setState({ platforms }))
+        this.props.platformInit()
+        this.props.gameInit()
     }
-
-    handleFieldChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
-    }
-
-    createPlatform = async (event) => {
-        event.preventDefault()
-
-        try {
-            const platformObject = {
-                name: this.state.name,
-                creator: this.state.creator,
-                year: this.state.year
-            }
-
-            const newPlatform = await platformService.create(platformObject)
-
-            this.setState({
-                platforms: this.state.platforms.concat(newPlatform),
-                name: '',
-                creator: '',
-                year: ''
-            })
-        } catch (exception) {
-            console.log(exception)
-        }
-    }
-
-    login = async (event) => {
-        event.preventDefault()
-
-        try {
-            const user = await loginService.login({
-                username: this.state.username,
-                password: this.state.password
-            })
-
-            window.localStorage.setItem('loggedinUser', JSON.stringify(user))
-            platformService.setToken(user.token)
-            console.log('Successfully logged in!')
-
-            this.setState({
-                user,
-                username: '',
-                password: ''
-            })
-
-            console.log(this.state.user)
-        } catch (exception) {
-            console.log('Login failed')
-        }
-    }
-
-    
 
     render() {
         return (
@@ -94,21 +26,7 @@ class App extends React.Component {
                 <Router>
                     <div>
                         <NavigationMenu />
-                        <Route exact path='/platforms' render={() => <PlatformsView 
-                            platforms={this.state.platforms}
-                            createPlatform={this.createPlatform}
-                            name={this.state.name}
-                            creator={this.state.creator}
-                            year={this.state.year}
-                            handleFieldChange={this.handleFieldChange}
-                        />} />
-                        <Route exact path='/games' render={() => <GamesList games={this.state.games} />} />
-                        <Route exact path='/login' render={() => <LoginForm 
-                            login={this.login}
-                            username={this.state.username}
-                            password={this.state.password}
-                            handleFieldChange={this.handleFieldChange}
-                        />} />
+                        <Route exact path='/games' render={() => <GamesList />} />
                     </div>
                 </Router>
             </Container>
@@ -116,4 +34,4 @@ class App extends React.Component {
     }
 }
 
-export default App
+export default connect(null, { platformInit, gameInit }) (App)
--- mvgc/src/components/GamesList.js
+++ mvgc/src/components/GamesList.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const GamesList = ({ games }) => {
-    return (
-        <div>
-            <h2>Games</h2>
-            <ul>
-                {games.map(game => <li key={game.id}>
-                    {game.name}
-                </li>)}
-            </ul>
-        </div>
-    )
+class GamesList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Games</h2>
+                <ul>
+                    {this.props.games.map(game => <li key={game.id}>{game.name}</li>)}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default GamesList
+const mapStateToProps = (state) => {
+    return {
+        games: state.games
+    }
+}
+
+const ConnectedGamesList = connect(mapStateToProps) (GamesList)
+
+export default ConnectedGamesList

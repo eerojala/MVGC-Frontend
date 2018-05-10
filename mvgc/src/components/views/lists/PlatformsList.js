02b6875f4104dea292f00b7b4e0bec0756e9e1d2
joq tyyppi <eero.ojala@helsinki.fi>
2018-05-10 20:15:15 +0300

    Changed PlatformList to use semantic ui Table component

--- mvgc/src/components/views/lists/PlatformsList.js
+++ mvgc/src/components/views/lists/PlatformsList.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Table } from 'semantic-ui-react'
 
 class PlatformsList extends React.Component {
     render() {
         return (
             <div>
                 <h2>Platforms</h2>
-                <ul>
-                    {this.props.platforms.map(platforms => <li key={platforms.id}>{platforms.name}</li>)}
-                </ul>
+                <Table>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell>Creator</Table.HeaderCell>
+                            <Table.HeaderCell>Year</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {this.props.platforms.map(platform => <Table.Row key={platform.id}>
+                            <Table.Cell>{platform.name}</Table.Cell>
+                            <Table.Cell>{platform.creator}</Table.Cell>
+                            <Table.Cell>{platform.year}</Table.Cell>
+                        </Table.Row>)}
+                    </Table.Body>
+                </Table>
             </div>
         )
     }

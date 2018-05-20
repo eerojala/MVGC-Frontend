87472486d0745ba27685327de8f0cf14fbb14011
joq tyyppi <eero.ojala@helsinki.fi>
2018-05-20 19:16:03 +0300

    Made userlist use a semantic ui table

--- mvgc/src/components/views/lists/UsersList.js
+++ mvgc/src/components/views/lists/UsersList.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Table } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 class UsersList extends React.Component {
     render() {
         return (
             <div>
                 <h2>Users</h2>
-                <ul>
-                    {this.props.users.map(user => <li key={user.id}>{user.username}</li>)}
-                </ul>
+                <Table>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Username</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {this.props.users.map(user => 
+                            <Table.Row key={user.id}>
+                                <Table.Cell>
+                                    <Link to={`/users/${user.id}`}>{user.username}</Link>
+                                </Table.Cell>
+                            </Table.Row> 
+                        )}
+                    </Table.Body>
+                </Table>
             </div>
         )
     }
